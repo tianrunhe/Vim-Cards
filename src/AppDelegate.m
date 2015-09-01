0712cb761cc2314989880487d2959a4a0641f639
Runhe(Bruce) Tian <tianrunhe@gmail.com>
2015-08-31 23:00:29 -0700

    Add PersisentStore in AppDelegate

--- src/AppDelegate.m
+++ src/AppDelegate.m
@@ -23,21 +23,12 @@
   self.window.backgroundColor = [UIColor whiteColor];
   CommandsCDTVC *rootViewController = [[CommandsCDTVC alloc] init];
 
-  NSFileManager *fileManager = [NSFileManager defaultManager];
-  NSURL *documentsDirectory =
-      [[fileManager URLsForDirectory:NSDocumentDirectory
-                           inDomains:NSUserDomainMask] firstObject];
-  NSURL *url = [documentsDirectory URLByAppendingPathComponent:@"coreData"];
-  UIManagedDocument *document = [[UIManagedDocument alloc] initWithFileURL:url];
-  NSManagedObjectContext *managedObject = document.managedObjectContext;
-
-  Command *command =
-      [NSEntityDescription insertNewObjectForEntityForName:@"Command"
-                                    inManagedObjectContext:managedObject];
+  Command *command = [NSEntityDescription
+      insertNewObjectForEntityForName:@"Command"
+               inManagedObjectContext:self.managedObjectContext];
   command.title = @"Ctrl-F";
-  command.content = @"Move one word forward";
-  //[document saveToURL:url forSaveOperation:UIDocumentSaveForOverwriting
-  //completionHandler:nil];
+  command.content = @"Move one word forward;";
+  [self saveContext];
 
   NSFetchRequest *request =
       [NSFetchRequest fetchRequestWithEntityName:@"Command"];
@@ -50,10 +41,11 @@
   ];
 
   NSFetchedResultsController *fetchedResultsController =
-      [[NSFetchedResultsController alloc] initWithFetchRequest:request
-                                          managedObjectContext:managedObject
-                                            sectionNameKeyPath:nil
-                                                     cacheName:nil];
+      [[NSFetchedResultsController alloc]
+          initWithFetchRequest:request
+          managedObjectContext:self.managedObjectContext
+            sectionNameKeyPath:nil
+                     cacheName:nil];
   rootViewController.fetchedResultsController = fetchedResultsController;
 
   UINavigationController *nav = [[UINavigationController alloc]
@@ -99,6 +91,114 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
   // Called when the application is about to terminate. Save data if
   // appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - Core Data stack
+
+@synthesize managedObjectContext = _managedObjectContext;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory {
+  // The directory the application uses to store the Core Data store file. This
+  // code uses a directory named "com.amazon.tianrunhe.Temp" in the
+  // application's documents directory.
+  return [[[NSFileManager defaultManager]
+      URLsForDirectory:NSDocumentDirectory
+             inDomains:NSUserDomainMask] lastObject];
+}
+
+- (NSManagedObjectModel *)managedObjectModel {
+  // The managed object model for the application. It is a fatal error for the
+  // application not to be able to find and load its model.
+  if (_managedObjectModel != nil) {
+    return _managedObjectModel;
+  }
+  NSURL *modelURL =
+      [[NSBundle mainBundle] URLForResource:@"coreData" withExtension:@"mom"];
+  _managedObjectModel =
+      [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+  return _managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+  // The persistent store coordinator for the application. This implementation
+  // creates and return a coordinator, having added the store for the
+  // application to it.
+  if (_persistentStoreCoordinator != nil) {
+    return _persistentStoreCoordinator;
+  }
+
+  // Create the coordinator and store
+
+  _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc]
+      initWithManagedObjectModel:[self managedObjectModel]];
+  NSURL *storeURL = [[self applicationDocumentsDirectory]
+      URLByAppendingPathComponent:@"coreData.sqlite"];
+  NSLog(@"Core Data store path = \"%@\"", [storeURL path]);
+  NSError *error = nil;
+  NSString *failureReason =
+      @"There was an error creating or loading the application's saved data.";
+  NSDictionary *options = @{
+    NSSQLitePragmasOption : @{@"journal_mode" : @"DELETE"}
+  };
+  if (![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType
+                                                 configuration:nil
+                                                           URL:storeURL
+                                                       options:options
+                                                         error:&error]) {
+    // Report any error we got.
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    dict[NSLocalizedDescriptionKey] =
+        @"Failed to initialize the application's saved data";
+    dict[NSLocalizedFailureReasonErrorKey] = failureReason;
+    dict[NSUnderlyingErrorKey] = error;
+    error =
+        [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:9999 userInfo:dict];
+    // Replace this with code to handle the error appropriately.
+    // abort() causes the application to generate a crash log and terminate. You
+    // should not use this function in a shipping application, although it may
+    // be useful during development.
+    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+    abort();
+  }
+
+  return _persistentStoreCoordinator;
+}
+
+- (NSManagedObjectContext *)managedObjectContext {
+  // Returns the managed object context for the application (which is already
+  // bound to the persistent store coordinator for the application.)
+  if (_managedObjectContext != nil) {
+    return _managedObjectContext;
+  }
+
+  NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+  if (!coordinator) {
+    return nil;
+  }
+  _managedObjectContext = [[NSManagedObjectContext alloc] init];
+  [_managedObjectContext setPersistentStoreCoordinator:coordinator];
+  return _managedObjectContext;
+}
+
+#pragma mark - Core Data Saving support
+
+- (void)saveContext {
+  NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+  if (managedObjectContext != nil) {
+    NSError *error = nil;
+    if ([managedObjectContext hasChanges] &&
+        ![managedObjectContext save:&error]) {
+      // Replace this implementation with code to handle the error
+      // appropriately.
+      // abort() causes the application to generate a crash log and terminate.
+      // You should not use this function in a shipping application, although it
+      // may be useful during development.
+      NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+      abort();
+    }
+  }
 }
 
 @end
