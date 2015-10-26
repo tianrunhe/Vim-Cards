8fa842378e1ec9342a0e159df4fa4b9bd98a939d
tianrunhe <tianrunhe@gmail.com>
2015-10-25 20:53:19 -0700

    Add new commands; Update upload_to_dynamodb script

--- src/scripts/upload_to_dynamodb.py
+++ src/scripts/upload_to_dynamodb.py
@@ -7,35 +7,46 @@ def main(overwrite=False, update=False):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Vim-commands')
     
-    updated = 0
-    inserted = 0
+    should_inserted = 0
     should_updated = 0
+    inserted = 0
+    updated = 0
 
     with open('Commands.csv', 'rb') as csvfile:
         command_reader = csv.reader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, skipinitialspace=True)
         for row in command_reader:
+            print "row = %s" % row
             title = row[0]
             usage = row[1]
             content = row[2]
             tags = re.split(',\s+', row[3])
             response = table.get_item(Key = {'title': title})
             item = {'title':title, 'usage':usage,'content':content,'tags':tags}
+            will_insert = False
             if 'Item' not in response: # new item
-                inserted = inserted + 1
-                should_updated = should_updated + 1
+                should_inserted = should_inserted + 1
+                will_insert = True
+                print "Should insert item as %s" % item
             elif item != response['Item']: # existing item and needs update
                 if not overwrite:
-                    item['tags'] = list(set(response['Item']['tag'] + tags))
-                updated = updated + 1
+                    item['tags'] = list(set(response['Item']['tags'] + tags))
                 should_updated = should_updated + 1
+                print "Should update item as %s" % item
+            else: # no differences
+                continue
 
-            print "Updating/Inserting item as %s" % item
             if update:
                 table.put_item(Item=item)
+                if will_insert:
+                    inserted = inserted + 1
+                    print "Inserted item as %s" % item
+                else:
+                    updated = updated + 1
+                    print "Updated item as %s" % item
         if update:
             print "Inserted %s row; Updated %s rows" % (inserted, updated)
         else:
-            print "%s rows needs to be updated/inserted." % should_updated
+            print "%s rows need to be updated; %s rows need to be inserted." % (should_updated, should_inserted)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Read a CSV file containing rows of Vim commands, and upload them to dynamodb table')
